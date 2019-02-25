fe253754d371a0c508996897d352708e05cc2431
JavierCenteno <jacenve@telefonica.net>
2019-02-25 09:43:46 +0100

    Added configuration loading

--- PAI1/main.py
+++ PAI1/main.py
@@ -6,3 +6,9 @@ This is the main file for this application.
 @author: Antonio Millán García
 """
 
+import codecs
+import json
+import os
+
+configuration = json.loads(codecs.open("configuration.json", "r", encoding="utf8").read())
+
