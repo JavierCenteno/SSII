7627ac61ef84f43c176fa656866a9d1c9a9e3976
JavierCenteno <jacenve@telefonica.net>
2019-02-25 10:05:45 +0100

    Added documentation and TODOs

--- PAI1/main.py
+++ PAI1/main.py
@@ -10,5 +10,13 @@ import codecs
 import json
 import os
 
+# Password, used to encrypt/decrypt hash files
+password = input("Please write your password")
+# Program configuration
 configuration = json.loads(codecs.open("configuration.json", "r", encoding="utf8").read())
+
+# TODO:
+# If this is the first time this script is executed, send a warning!
+# So that an attacker deletes the hash file, it is detected
+# The warning should warn that the script is running for the first time and that if it's not, then the hashes have been deleted and there's a possibility that the files have been tampered with
 
