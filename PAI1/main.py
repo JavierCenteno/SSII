36e05ceb8450ee5dce821c7d0e79daa58022ef3a
JavierCenteno <jacenve@telefonica.net>
2019-02-25 10:10:33 +0100

    Formatting

--- PAI1/main.py
+++ PAI1/main.py
@@ -11,12 +11,11 @@ import json
 import os
 
 # Password, used to encrypt/decrypt hash files
-password = input("Please write your password")
+password = input("Please write your password: ")
 # Program configuration
 configuration = json.loads(codecs.open("configuration.json", "r", encoding="utf8").read())
 
 # TODO:
-# If this is the first time this script is executed, send a warning!
-# So that an attacker deletes the hash file, it is detected
-# The warning should warn that the script is running for the first time and that if it's not, then the hashes have been deleted and there's a possibility that the files have been tampered with
+# If the hashes file doesn't exist, it may mean either this is the first time this script is being run or an attacker has deleted the hashes. Send a warning!
+# The warning should warn that the script is running for the first time and that if it's not, then the hashes have been deleted and there's a possibility that the files have been tampered with.
 
