444469ce7efc51105d42fa6ee070d9a06e689302
JavierCenteno <jacenve@telefonica.net>
2019-03-03 15:02:42 +0100

    Added load and save functions

--- PAI1/main.py
+++ PAI1/main.py
@@ -15,19 +15,39 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 
+# Path to the configuration file
+configuration_file_path = "configuration.json"
+
+# Path to the file which stores the encrypted hashes of files
+hashes_file_path = "hashes"
+
 # Password, used to encrypt/decrypt hash files
 password = input("Please write your password: ").encode()
+
 # Program configuration
-configuration = json.loads(codecs.open("configuration.json", "r", encoding="utf8").read())
+configuration = json.loads(codecs.open(configuration_file_path, "r", encoding="utf8").read())
+
 # Create key
-encryption_kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=b'salt_', iterations=100000, backend=default_backend())
+encryption_kdf = PBKDF2HMAC(algorithm = hashes.SHA256(), length = 32, salt = b"salt_", iterations = 100000, backend = default_backend())
 encryption_key = base64.urlsafe_b64encode(encryption_kdf.derive(password))
 encryption_fernet = Fernet(key)
 
-# TODO:
-# encrypt using encryption_fernet.encrypt(data)
-# decrypt using encryption_fernet.decrypt(data)
-# can pass the encrypt and decrypt functions data straight from open(filename, "r").read(), and write that data straight to open(filename, "w").write()
+# Loads and decrypts the hashes file.
+def load_hashes():
+	file = open(hashes_file_path, "r")
+	encrypted_data = file.read()
+	data = encryption_fernet.decrypt(encrypted_data)
+	file.close()
+	return data
+
+# Encrypts and saves the hashes file.
+def save_hashes(hashes):
+	file = open(hashes_file_path, "w")
+	encrypted_data = encryption_fernet.encrypt(hashes)
+	file.write(encrypted_data)
+	file.close()
+
+
 
 # TODO:
 # If the hashes file doesn't exist, it may mean either this is the first time this script is being run or an attacker has deleted the hashes. Send a warning!
