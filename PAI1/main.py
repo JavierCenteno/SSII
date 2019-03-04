0af5f41d46e6761928f9033282f9ba71b413277f
JavierCenteno <jacenve@telefonica.net>
2019-03-04 09:01:05 +0100

    Generalized functions and added settings

--- PAI1/main.py
+++ PAI1/main.py
@@ -19,7 +19,10 @@ from cryptography.fernet import Fernet
 configuration_file_path = "configuration.json"
 
 # Path to the file which stores the encrypted hashes of files
-hashes_file_path = "hashes"
+hashes_file_path = None
+
+# Path to the file which stores the security warnings
+security_file_path = None
 
 # Password, used to encrypt/decrypt hash files
 password = input("Please write your password: ").encode()
@@ -27,32 +30,46 @@ password = input("Please write your password: ").encode()
 # Program configuration
 configuration = json.loads(codecs.open(configuration_file_path, "r", encoding="utf8").read())
 
+hashes_file_path = configuration["hashes_file_path"]
+security_file_path = configuration["security_file_path"]
+
 # Create key
 encryption_kdf = PBKDF2HMAC(algorithm = hashes.SHA256(), length = 32, salt = b"salt_", iterations = 100000, backend = default_backend())
 encryption_key = base64.urlsafe_b64encode(encryption_kdf.derive(password))
 encryption_fernet = Fernet(key)
 
-# Loads and decrypts the hashes file.
-def load_hashes():
-	file = open(hashes_file_path, "r")
+# Functions to load and save encrypted files, encrypting or decrypting the data to be saved in the process
+
+def load_encrypted_file(file_path):
+	file = open(file_path, "r")
 	encrypted_data = file.read()
 	data = encryption_fernet.decrypt(encrypted_data)
 	file.close()
 	return data
 
-# Encrypts and saves the hashes file.
-def save_hashes(hashes):
-	file = open(hashes_file_path, "w")
-	encrypted_data = encryption_fernet.encrypt(hashes)
+def save_encrypted_file(file_path, data):
+	file = open(file_path, "w")
+	encrypted_data = encryption_fernet.encrypt(data)
 	file.write(encrypted_data)
 	file.close()
 
-hashes = None
+old_hashes = None
 
 if not os.path.isfile(hashes_file_path):
 	# If the hashes file doesn't exist, it may mean either this is the first time this script is being run or an attacker has deleted the hashes.
 	print("The hash file doesn't exist. If this is not the first time you've run this program, it means it's been deleted and your files may have been tampered with.")
 else:
-	hashes = load_hashes()
+	old_hashes = load_encrypted_file(hashes_file_path)
 
+if not os.path.isfile(security_file_path):
+	# If the security file doesn't exist, it may mean either this is the first time this script is being run or an attacker has deleted the file.
+	print("The security file doesn't exist. If this is not the first time you've run this program, it means it's been deleted and your files may have been tampered with.")
+else:
+	old_hashes = load_hashes(security_file_path)
 
+# TODO: hash files and store the hashes in new_hashes
+new_hashes = None
+
+# TODO: compare old_hashes and new_hashes
+
+# TODO: Give 2-3 options at least for hash algorithms
