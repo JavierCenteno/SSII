2b7ef3c89501d0fb98d44e8e1c9d912fa799e107
JavierCenteno <jacenve@telefonica.net>
2019-03-09 19:27:52 +0100

    Code cleanup and minor bug fixes

--- PAI1/main.py
+++ PAI1/main.py
@@ -22,23 +22,18 @@ import threading
 # Path to the configuration file
 configuration_file_path = "configuration.json"
 
-# Path to the file which stores the encrypted hashes of files
-
-
-# Path to the file which stores the security warnings
-
-
 # Password, used to encrypt/decrypt hash files
 password = input("Please write your password: ").encode()
 
 # Program configuration
 configuration = json.loads(codecs.open(configuration_file_path, "r", encoding="utf8").read())
 
+# Load configuration
 hashes_file_path = configuration["hashes_file_path"]
 security_file_path = configuration["security_file_path"]
 scanned_directories = configuration["scanned_directories"]
-check_frequency=int(configuration["check_frequency"])
-report_frequency=int(configuration["report_frequency"])
+check_frequency = int(configuration["check_frequency"])
+report_frequency = int(configuration["report_frequency"])
 
 # Create key
 encryption_kdf = PBKDF2HMAC(algorithm = hashes.SHA256(), length = 32, salt = b"salt_", iterations = 100000, backend = default_backend())
@@ -62,28 +57,28 @@ algorithm = algorithms[configuration["algorithm"]]
 def load_encrypted_file(file_path):
 	if os.stat(file_path).st_size != 0:
 		file = open(file_path, "rb")
-		encrypted_data =file.read()
+		encrypted_data = file.read()
 		data = encryption_fernet.decrypt(encrypted_data)
 		file.close()
 	else:
-		data=""
+		data = ""
 	return data
 
 def save_encrypted_file(file_path, list):
 	file = open(file_path, "wb")
-	data=""
+	data = ""
 	for l in list:
-		data=data+str(l)+"\n"
+		data = data + str(l) + "\n"
 	encrypted_data = encryption_fernet.encrypt(data.encode())
 	file.write(encrypted_data)
 	file.close()
 # TODO: hash files and store the hashes in new_hashes
 
 def hash_file(file):
-	hash= algorithm
+	hash = algorithm
 	with open(file, "rb") as f:
-		#reads file in chunks of 4096 bytes, doesn't have to load whole file on memory, which can be problematic with very big files.
-		for chunk in iter(lambda:f.read(4096),b""):
+		# Reads file in chunks of 4096 bytes, doesn't have to load whole file on memory, which can be problematic with very big files.
+		for chunk in iter(lambda: f.read(4096), b""):
 			hash.update(chunk)
 	return hash.hexdigest()
 
@@ -96,10 +91,9 @@ def load_new_hashes():
 				hashes_list.append(hash_file(file))
 	return hashes_list
 
-def load_hashes():
-	hashes=[]
-
-	f=str(load_encrypted_file(hashes_file_path))
+def load_old_hashes():
+	hashes = []
+	f = str(load_encrypted_file(hashes_file_path))
 	print(f)
 	for line in f:
 		hashes.append(line)
@@ -117,10 +111,10 @@ if not os.path.isfile(security_file_path):
 	# If the security file doesn't exist, it may mean either this is the first time this script is being run or an attacker has deleted the file.
 	print("The security file doesn't exist. If this is not the first time you've run this program, it means it's been deleted and your files may have been tampered with.")
 else:
-	old_hashes = load_hashes()
+	old_hashes = load_old_hashes()
 
 def load_file_names():
-	names=[]
+	names = []
 	for directory in scanned_directories:
 		for file in os.listdir(directory):
 			names.append(file)
@@ -128,16 +122,16 @@ def load_file_names():
 
 def main_loop():
 	while True:
-		old_hashes = load_hashes()
+		old_hashes = load_old_hashes()
 		new_hashes = load_new_hashes()
 		filenames = load_file_names()
 		n = 0
-		print("Los nuevos hashes son: "+ str(new_hashes))
-		print("Los viejos hashes son: "+ str(old_hashes))
-		for i in old_hashes:
+		print("New hashes: " + str(new_hashes))
+		print("Old hashes: " + str(old_hashes))
+		for i in range(len(old_hashes)):
 			if new_hashes[i] != old_hashes[i]:
 				file = open(security_file_path, "w")
-				file.write(datetime.datetime.now()+"|NEW INCIDENCE|"+filenames[i]+" has been modified without permission.")
+				file.write(datetime.datetime.now() + "|NEW INCIDENCE|" + filenames[i] + " has been modified without permission.")
 			n = n + 1
 		save_encrypted_file(hashes_file_path,new_hashes)
 		# Time check of configuration.json
