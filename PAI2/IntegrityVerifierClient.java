ecc6e3d083c947f17f93f7f0913c489c5e93721f
JavierCenteno <jacenve@telefonica.net>
2019-03-11 09:53:36 +0100

    Code cleanup

--- PAI2/IntegrityVerifierClient.java
+++ PAI2/IntegrityVerifierClient.java
@@ -1,11 +1,15 @@
+package src;
+
 import java.io.*;
+import java.net.Socket;
+
 import javax.swing.*;
 import javax.net.*;
 
 public class IntegrityVerifierClient {
 
 	////////////////////////////////////////////////////////////////////////////////
-	// Instance initialiers
+	// Instance initializers
 
 	/**
 	 * Constructs a client and opens a connection.
@@ -13,14 +17,15 @@ public class IntegrityVerifierClient {
 	public IntegrityVerifierClient() {
 		try {
 			SocketFactory socketFactory = (SocketFactory) SocketFactory.getDefault();
-			Socket socket = (Socket) socketFactory.createSocket("localhost", 7070 );
+			Socket socket = (Socket) socketFactory.createSocket("localhost", 7070);
 			// Create a PrintWriter to sent messages and MACs to the server
 			PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-			String userName = JOptionPane.showInputDialog(null,"Introduzca su mensaje:");
+			String mensaje = JOptionPane.showInputDialog(null, "Introduzca su mensaje: ");
 			// Send message to the server
 			output.println(mensaje);
 			// Calculate the MAC with shared key
-			output.println(macdelMensaje);
+			String messageMAC = "";// TODO: calculate MAC
+			output.println(messageMAC);
 			// Flush operations to send messages correctly
 			output.flush();
 			// Create a BufferedReader to read the response to the server
--- PAI2/IntegrityVerifierServer.java
+++ PAI2/IntegrityVerifierServer.java
@@ -1,3 +1,15 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.net.ServerSocketFactory;
+
 /**
  * Defines a server that receives messages and checks their integrity.
  */
@@ -9,7 +21,7 @@ public class IntegrityVerifierServer {
 	private ServerSocket serverSocket;
 
 	////////////////////////////////////////////////////////////////////////////////
-	// Instance initialiers
+	// Instance initializers
 
 	/**
 	 * Constructs a server.
@@ -28,7 +40,12 @@ public class IntegrityVerifierServer {
 	 * Instances a server and runs it.
 	 */
 	public static void main(String[] args) {
-		IntegrityVerifierServer server = new IntegrityVerifierServer();
+		IntegrityVerifierServer server = null;
+		try {
+			server = new IntegrityVerifierServer();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		server.runServer();
 	}
 
@@ -38,21 +55,22 @@ public class IntegrityVerifierServer {
 	/**
 	 * Execution of the server to listen the clients.
 	 */
-	private void runServer(){
+	private void runServer() {
 		while (true) {
 			// Hold on the request of the clients to check messages and MACs
 			try {
-				System.err.println( "Waiting clients connections...");
+				System.err.println("Waiting clients connections...");
 				Socket socket = (Socket) serverSocket.accept();
 				// Open a BufferedReader to read from the client
 				BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				// Open a PrintWriter to send data to the clients
-				PrintWriter  output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-				// Read client’s message and its MAC
-				String mensaje = input.readLine();
-				// Calculate MAC of the message
-				String macMensajeEnviado = input.readLine();
-				if (macMensajeEnviado.equals(macdelMensajeCalculado)) {
+				PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+				// Read client's message and its MAC
+				String message = input.readLine();
+				// Compute MAC of the message
+				String computedMessageMAC = "";// TODO: compute MAC
+				String sentMessageMAC = input.readLine();
+				if (sentMessageMAC.equals(computedMessageMAC)) {
 					output.println("Message sent with integrity");
 				} else {
 					output.println("Message sent with NO integrity.");
@@ -60,7 +78,7 @@ public class IntegrityVerifierServer {
 				output.close();
 				input.close();
 				socket.close();
-			} catch ( IOException ioException ) {
+			} catch (IOException ioException) {
 				ioException.printStackTrace();
 			}
 		}
