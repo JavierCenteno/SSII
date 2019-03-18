d47862358368b25f40f4cb971df3c3e786749105
JavierCenteno <jacenve@telefonica.net>
2019-03-18 10:02:18 +0100

    bug fixes

--- PAI2/Util.java
+++ PAI2/Util.java
@@ -5,24 +5,10 @@ package src;
  */
 public class Util {
 
-	/**
-	 * Converts an integer in the range [0, 15] to an hexadecimal character.
-	 */
-	private static char intToHex(int i) {
-		if (0 <= i && i <= 9) {
-			return (char) (i + '0');
-		} else if (10 <= i && i <= 15) {
-			return (char) (i - 10 + 'A');
-		} else {
-			throw new IllegalArgumentException();
-		}
-	}
-
 	public static String fromByteArray(byte[] bytes) {
 		StringBuilder stringBuilder = new StringBuilder(bytes.length * 2);
 		for (int index = 0; index < bytes.length; ++index) {
-			stringBuilder.append(intToHex(bytes[index] & 0xF));
-			stringBuilder.append(intToHex(bytes[index] >>> 4));
+			stringBuilder.append(Integer.toHexString(((int) bytes[index]) & 0xFF));
 		}
 		return stringBuilder.toString();
 	}
